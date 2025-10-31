39900edf1489b39a814e5e2d53fae925ffda07b1
Samuel <samuelarmenta79@yahoo.com>
2025-10-31 16:18:42 -0600

    Add server and connect to DB finish up rsvp form

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+var db = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'EightEight26!',
+    database:'wedding_guest'
+})
+
+app.post('/addGuest', (req, res) => {
+    const mainGuestName = req.body.mainGuestName;
+    const guest = req.body.name;
+    const phoneNumber = req.body.phoneNumber;
+    const address = req.body.address;
+    const email = req.body.email;
+    db.query("INSERT INTO guestinformation (mainGuestName, guest, phoneNumber, address, email) VALUES (?,?,?,?,?)",
+        [mainGuestName, guest, phoneNumber, address, email], 
+        (err, result) =>{
+        if (err) {
+            console.log(err);
+        } else {
+            res.send({mainGuestName: mainGuestName})
+       }
+    })                                          
+})
+
+app.listen(8080, () =>{
+    console.log('Server listening on port 8080')
+});
+
--- wedding-app/src/rsvpForm/rsvpForm.js
+++ wedding-app/src/rsvpForm/rsvpForm.js
@@ -1,37 +1,75 @@
 import React, { useState } from 'react';
 import './rsvpForm.css';
+import axios from 'axios'
 
-function RSVPForm({onClose}) {
+function RSVPForm({ onClose }) {
 
-    const [guest, setGuest] = useState([{value: ''}]);
-    const [close, setClose] = useState (false);
+    const [guest, setGuest] = useState([{ name: '' }]);
+    const [phoneNumber, setPhoneNumber] = useState({ phoneNumber: '' });
+    const [address, setaddress] = useState({ address: '' });
+    const [email, setEmail] = useState({ email: '' });
+    const [close, setClose] = useState(false);
 
-    const handleAddField = () => {
-      setGuest([...guest, { value: '' }]);
+    const handleAddName = () => {
+        setGuest([...guest, { name: '' }]);
     };
 
-    const handleRemoveField = (index) => {
-      const newFields = guest.filter((_, i) => i !== index);
-      setGuest(newFields);
+    const handleRemoveName = (index) => {
+        const removeName = guest.filter((_, i) => i !== index);
+        setGuest(removeName);
     };
 
-    const handleInputChange = (e, index) => {
-      const newFields = [...guest];
-      newFields[index].value = e.target.value;
-      setGuest(newFields);
+    const handleNameInput = (e, index) => {
+        const updateName = [...guest];
+        updateName[index].name = e.target.value;
+        setGuest(updateName);
+    };
+
+    const handlePhoneNumber = (e) => {
+        const phoneNumber = e.target.value;
+        setPhoneNumber(phoneNumber);
+    };
+
+    const handleAdress = (e) => {
+        const address = e.target.value;
+        setaddress(address);
+    };
+
+    const handleEmail = (e) => {
+        const email = e.target.value;
+        setEmail(email);
     };
 
     const handleSetClose = () => {
+        let mainGuestName = guest[0].name;
+
+        Object.values(guest).forEach(guestName => {
+            const name = guestName.name;
+            axios.post('http://localhost:8080/addGuest', { mainGuestName: mainGuestName, name: name, phoneNumber: phoneNumber,
+                 address:address, email:email })
+                .then((data) => {
+                    console.log(data);
+                });
+        });
+
         onClose();
         setClose(!close);
     }
-    
-    return  ( !close &&
+
+    const requiredField = () => {
+        console.log('Wht is this value, ', (guest[0].name.length ))
+        console.log('Wht is this value, ', (phoneNumber.length))
+        console.log('Wht is this value, ', ( address.length))
+        console.log('Wht is this value, ', (email.length))
+        return (guest[0].name.length && phoneNumber.length && address.length && email.length) && true;
+    }
+
+    return (!close &&
         <div className='form'>
-            <div className = 'form-content'>
+            <div className='form-content'>
 
                 {/* exit button  */}
-                 <button className ='form-exit-button' onClick={() => handleSetClose() }>X</button>
+                <button className='form-exit-button' onClick={() => handleSetClose()}>X</button>
 
                 <div className='form-welcomeMessage'>
                     <p>Thank you for attending our wedding, you mean so much to us!<br />
@@ -43,27 +81,28 @@ function RSVPForm({onClose}) {
                         <label >
                             Name of guest:
                         </label>
-                        <button className='form-addNameButton' onClick = {handleAddField}>Add</button>
-                    </div>    
-                    
+                        <button className='form-addNameButton' onClick={handleAddName}>Add</button>
+                    </div>
+
                     <div >
                         {guest.map((field, index) => (
-                            <div  className = 'form-nameInput' key={index}>
-                               
+                            <div className='form-nameInput' key={index}>
+
                                 <input
                                     className='form-TextInput'
+                                    placeholder='First name Last Name'
                                     type="text"
                                     value={field.value}
-                                    onChange={(e) => handleInputChange(e, index)}
+                                    onChange={(e) => handleNameInput(e, index)}
                                 />
-                                
-                                <button 
-                                type="button" 
-                                className = 'form-name-deleteButton'
-                                onClick={() => handleRemoveField(index)}>
+
+                                <button
+                                    type="button"
+                                    className='form-name-deleteButton'
+                                    onClick={() => handleRemoveName(index)}>
                                     X
                                 </button>
-                                
+
                             </div>
                         ))}
                     </div>
@@ -72,25 +111,45 @@ function RSVPForm({onClose}) {
                         <label>
                             Phone Number:
                         </label>
-                        <input className='form-TextInput' type="text"></input>
+                        <input
+                        placeholder='(000)-000-0000'
+                            className='form-TextInput'
+                            type="text"
+                            onChange={(e) => handlePhoneNumber(e)}
+                        />
                     </div>
 
                     <div className='form-PhoneNumber'>
                         <label>
                             Address:
                         </label>
-                        <input className='form-TextInput' type="text"></input>
+                        <input
+                            placeholder='Address City State Zip'
+                            className='form-TextInput'
+                            type="text"
+                            onChange={(e) => handleAdress(e)}
+                        />
                     </div>
 
                     <div className='form-email'>
                         <label>
                             Email:
                         </label>
-                        <input className='form-TextInput' type="text"></input>
+                        <input
+                            placeholder='CantWaitToSeeYou@GT.com'
+                            className='form-TextInput'
+                            type="email"
+                            onChange={(e) => handleEmail(e)}
+                        />
                     </div>
 
-                    <div className='form-submit'>    
-                    <button className ='form-submit-button' onClick={() =>handleSetClose(!close)}>Submit</button>
+                    <div className='form-submit'>
+                        <button 
+                        className= {requiredField() 
+                            ? 'form-submit-button'
+                            : 'form-notvalid-submit-button' 
+                            } 
+                        onClick={() => handleSetClose(!close)}>Submit</button>
                     </div>
 
                 </div>
